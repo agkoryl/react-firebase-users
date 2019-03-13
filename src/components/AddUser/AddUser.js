304dfb3252279e35744555c1d4e037a50c8201cc
Agnieszka Koryl <ag.koryl@gmail.com>
2019-03-13 21:03:18 +0100

    user add working

--- src/components/AddUser/AddUser.js
+++ src/components/AddUser/AddUser.js
@@ -1,29 +1,55 @@
-import React from 'react';
+import React from "react";
 import { Button, Container, Form, Modal } from "semantic-ui-react";
 
-import './AddUser.css';
+import db from "../../database/firebase";
+import "./AddUser.css";
 
+class AddUser extends React.Component {
+  state = {
+    firstName: "",
+    lastName: "",
+    age: null
+  }
 
-function AddUser(props) {
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  handleAddUserToFirebase = event => {
+    db.ref('/users').push(this.state);
+  };
+
+  componentDidMount() {
+      console.log(this.state);
+  }
+
+  render() {
     return (
-        <Modal trigger={<Button className="green medium add-button">Add a new user</Button>}>
+        <Modal
+          trigger={
+            <Button className="green medium add-button">Add a new user</Button>
+          }
+        >
           <Modal.Header>Add a new user</Modal.Header>
           <Modal.Content>
             <Container className="text container">
               <Form>
                 <Form.Field>
                   <label>First Name</label>
-                  <input placeholder="First Name" />
+                  <input placeholder="First Name" name="firstName" onChange={this.handleChange} />
                 </Form.Field>
                 <Form.Field>
                   <label>Last Name</label>
-                  <input placeholder="Last Name" />
+                  <input placeholder="Last Name" name="lastName" onChange={this.handleChange} />
                 </Form.Field>
                 <Form.Field>
                   <label>Age</label>
-                  <input placeholder="Age" />
+                  <input placeholder="Age"  name="age" onChange={this.handleChange} />
                 </Form.Field>
-                <Button type="submit" className="green medium">
+                <Button
+                  className="green medium"
+                  onClick={this.handleAddUserToFirebase}
+                >
                   Add
                 </Button>
               </Form>
@@ -31,6 +57,7 @@ function AddUser(props) {
           </Modal.Content>
         </Modal>
     )
-  }
+ 
+  }}
 
-  export default AddUser;
+export default AddUser;
